008d0add0b82749c1135623e6425d537b2fe74ca
Ivan Xamarin <ivanxamarin032019@hotmail.com>
2020-08-26 00:31:20 +0200

    Empezando la Parte 32

--- OnSale.Prism/OnSale.Prism/App.xaml.cs
+++ OnSale.Prism/OnSale.Prism/App.xaml.cs
@@ -1,9 +1,12 @@
-using Prism;
-using Prism.Ioc;
+using OnSale.Common.Services;
 using OnSale.Prism.ViewModels;
 using OnSale.Prism.Views;
-using Xamarin.Essentials.Interfaces;
+using Prism;
+using Prism.Ioc;
+using Syncfusion.Licensing;
+using System;
 using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
 using Xamarin.Forms;
 
 namespace OnSale.Prism
@@ -17,17 +20,26 @@ namespace OnSale.Prism
 
         protected override async void OnInitialized()
         {
+            SyncfusionLicenseProvider.RegisterLicense("MzA4NTUxQDMxMzgyZTMyMmUzMGdtZmtxYXc5MmVvVURoTHBXaWFPY2hLbWtaTVJmRDl1eUVodWw1V1NQNWM9");
+
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync($"NavigationPage/{nameof(ProductsPage)}");
+        }
+
+        private object Products()
+        {
+            throw new NotImplementedException();
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+            containerRegistry.Register<IApiService, ApiService>();
 
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<ProductsPage, ProductsPageViewModel>();
         }
     }
 }
