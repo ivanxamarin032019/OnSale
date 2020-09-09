edd06bb4f3b0b40c4356e755fc91947177539f90
ivanXam Xam <iva29234azur1@hotmail.com>
2020-09-09 14:06:49 +0200

    Terminamos las API's. Empiezo la parte 51 "PaymentMethod"

--- OnSale/OnSale.Web/Controllers/AccountController.cs
+++ OnSale/OnSale.Web/Controllers/AccountController.cs
@@ -312,13 +312,13 @@ namespace OnSale.Web.Controllers
             return View(model);
         }
 
-        public IActionResult ChangePassword()
+        public IActionResult ChangePasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        public async Task<IActionResult> ChangePasswordMVC(ChangePasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -366,13 +366,13 @@ namespace OnSale.Web.Controllers
             return View();
         }
 
-        public IActionResult RecoverPassword()
+        public IActionResult RecoverPasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> RecoverPassword(RecoverPasswordViewModel model)
+        public async Task<IActionResult> RecoverPasswordMVC(RecoverPasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
--- OnSale/OnSale.Web/Helpers/IBlobHelper.cs
+++ OnSale/OnSale.Web/Helpers/IBlobHelper.cs
@@ -4,13 +4,16 @@ using System.Threading.Tasks;
 
 namespace OnSale.Web.Helpers
 {
+    //esta clase esta Sobrecargarda con 3 métodos
     public interface IBlobHelper
     {
+        // La usamos desde la interface de usuario
         Task<Guid> UploadBlobAsync(IFormFile file, string containerName);
 
+        // La usamos cuando usamos el API
         Task<Guid> UploadBlobAsync(byte[] file, string containerName);
-
+        
+        //Esta la usa el SeedDb
         Task<Guid> UploadBlobAsync(string image, string containerName);
     }
-
 }
